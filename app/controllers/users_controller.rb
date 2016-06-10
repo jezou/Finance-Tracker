e0addccd3dd975c271adf1c779b0331879899d74
jezou <jefferson.zou@yale.edu>
2016-06-10 16:31:28 +0000

    Add friends and search for friends

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,30 @@ class UsersController < ApplicationController
   end
 
   def my_friends
+    @friends = current_user.friends
+  end
+
+  def search_friends
+    @users = User.search(params[:search_param])
+    logger.info("Test")
+    if @users
+      @users = current_user.except_current_user(@users)
+      render partial: "friends/lookup"
+    else
+      render status: :not_found, nothing:true
+    end
+  end
+
+  def add_friend
+    @friend = User.find(params[:friend])
+    current_user.friendships.build(friend_id: @friend.id)
+    if current_user.save
+      flash[:notice] = "Friend was successfully added"
+      redirect_to my_friends_path
+    else
+      flash[:error] = "There was an error adding this user as a friend"
+      redirect_to my_friends_path
+    end
   end
 
 end
